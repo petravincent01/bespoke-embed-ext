79576971e55810c6809839762c93a3c38b4dbe5f
Shirley <shirley1268@gmail.com>
2023-01-21 19:39:37 -0500

    test iframe

--- src/sections/FirstSection.js
+++ src/sections/FirstSection.js
@@ -6,8 +6,10 @@ export default function FirstSection({ children }) {
       <div className="wrapper">
         <div className="content-grid">
           <div className="quarterly-container">
-            {/* <iframe sandbox="allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-scripts allow-same-origin allow-popups allow-modals allow-popups-to-escape-sandbox" webkitallowfullscreen mozallowfullscreen allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen="true"  src="https://www.paperturn-view.com/global-publishing-mck/mckinsey-quarterly?pid=Mjk296702&v=1.1"></iframe> */}
-            <iframe src="https://www.paperturn-view.com/global-publishing-mck/mckinsey-quarterly?pid=Mjk296702&v=1.1" width="100%" height="408"  allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" className="paperTurn-iframe"></iframe>
+           
+            <iframe sandbox="allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-scripts allow-same-origin allow-popups allow-modals allow-popups-to-escape-sandbox" webkitallowfullscreen mozallowfullscreen allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen="true"  src="https://www.paperturn-view.com/global-publishing-mck/mckinsey-quarterly?pid=Mjk296702&v=1.1"></iframe>
+        
+            
            </div>
           </div>  
         </div>
