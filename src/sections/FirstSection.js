e46492298d64e037857e8d649a1f99490e61bece
Shirley <shirley1268@gmail.com>
2023-01-21 22:13:06 -0500

    add back some iframe classes

--- src/sections/FirstSection.js
+++ src/sections/FirstSection.js
@@ -9,9 +9,7 @@ export default function FirstSection({ children }) {
             <iframe sandbox="allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-scripts allow-same-origin allow-popups allow-modals allow-popups-to-escape-sandbox" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen="true"  src="https://www.paperturn-view.com/global-publishing-mck/mckinsey-quarterly?pid=Mjk296702&v=1.1"></iframe>
            </div>
           </div>
-            
         </div>
-
     </section>
   );
 }
