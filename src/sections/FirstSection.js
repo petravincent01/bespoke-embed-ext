07d2c7d6140b9fd1caba78b516b6fc567583f972
Shirley S <you@example.com>
2023-01-24 13:35:48 -0500

    csS

--- src/sections/FirstSection.js
+++ src/sections/FirstSection.js
@@ -4,12 +4,12 @@ import {isMobile, isTablet} from 'react-device-detect';
 const MyComponent = () => {
   if(isMobile|| isTablet) {
       return (
-        <a href="https://digitalq.mckinsey.com/?v=5.1" target="_blank">View full screen for navigation and accessibility options (including text size)</a> 
+        <a href="https://digitalq.mckinsey.com/?v=5.1" target="_blank">Click here for navigation and accessibility options (including text size)</a> 
       )
     }
     else{  
       return (
-        <a href="https://digitalq.mckinsey.com/?v=5.1" target="_self">View full screen for navigation and accessibility options (including text size)</a> 
+        <a href="https://digitalq.mckinsey.com/?v=5.1" target="_self">Click here for navigation and accessibility options (including text size)</a> 
        )
     }
 };
