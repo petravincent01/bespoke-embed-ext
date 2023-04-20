9a76a699887619ef2ca2459cdde2b2d655c0a258
Petra Vincent <68263791+petravincent01@users.noreply.github.com>
2023-04-19 20:24:24 -0400

    background

--- src/sections/FirstSection.js
+++ src/sections/FirstSection.js
@@ -95,10 +95,10 @@ export default function FirstSection({ children }) {
           <div className="quarterly-container">
 
             {/* default paper turn for sample */ }
-            {/* <iframe src="https://www.paperturn-view.com/?pid=Mjg288829&v=5.1&bgcolor=%23051c2c&embed=script&shadow=&flipSound=&hardCover=" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" className="paperTurn-iframe"></iframe> */}
+            {/* <iframe src="https://www.paperturn-view.com/?pid=Mjg288829&v=5.1&bgcolor=%23DDDDDD&embed=script&shadow=&flipSound=&hardCover=" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" className="paperTurn-iframe"></iframe> */}
             {/* Full edition for after users sign in */ }
            
-               <iframe src="https://www.paperturn-view.com/?pid=Mjk296702&v=1.1&bgcolor=%23051c2c&embed=iframe&shadow=&flipSound=&hardCover=" width="100%" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" className="paperTurn-iframe"></iframe>
+               <iframe src="https://www.paperturn-view.com/?pid=Mjk296702&v=1.1&bgcolor=%23DDDDDD&embed=iframe&shadow=&flipSound=&hardCover=" width="100%" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" className="paperTurn-iframe"></iframe>
 
             
             {/* <iframe sandbox="allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-scripts allow-same-origin allow-popups allow-modals allow-popups-to-escape-sandbox" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen="true"  src="https://www.paperturn-view.com/global-publishing-mck/mckinsey-quarterly-vol-59-no-1?pid=Mjg288829&v=5.1"></iframe> */}
